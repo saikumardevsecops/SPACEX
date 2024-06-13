0d757bf12171ae09baf2cb15f907ee61526dedd1
unknown <saikumar@gmail.com>
2024-06-13 22:20:58 +0530

    restart

---
+++ retart_service_any.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set +x
+if [ $# -gt 0 ]; then
+    for REGION in $*; do
+        cowsay "[#-------${REGION}-------#]"
+        aws ec2 describe-vpcs --region $REGION | jq ".Vpcs[].VpcId" -r
+    done
+else
+    cowsay "You Have Given $# paramters to this script, please provide arg Eg.us-east-1."
+fi
